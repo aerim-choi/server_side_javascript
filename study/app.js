edcb1091feb009d7c1c12c3637282c2bfd5425a8
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-26 13:26:18 +0900

    섹션6. Express,템플릿엔진
    - 섹션6. Express,템플릿엔진 실습코드입니다.

--- study/app.js
+++ study/app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
-
+app.locals.pretty=true;
+ //pug 템플릿 엔진과 express를 연결
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 
+app.get('/template',function(req,res){ 
+    res.render('temp',{time:Date(),_title:'Jade'});      //템플릿엔진의 코드에 따라서 만들어진 템플릿파일을 읽어오는 메소드render()
+})                          //temp이름의 템플릿 파일을 웹페이지로 렌더링해서 사용자에게 응답한다.
 app.get('/route',function(req,res){
     res.send('Hello Router,<img src="/rubberduck.jpeg">')
 })
